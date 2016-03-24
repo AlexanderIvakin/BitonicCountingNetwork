6b9f4ea68826cedacaf0f6e25464e5c4f12f7251
Alexander Ivakin <alex.ivakin@icloud.com>
2016-03-24 19:19:01 +0100

    Initial commit

---
+++ BitonicCountingNetwork/Balancer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitonicCountingNetwork
+{
+    public class Balancer
+    {
+        private readonly object _lock = new object();
+
+        private bool _toggle = true;
+
+        public int traverse()
+        {
+            lock (_lock)
+            {
+                try
+                {
+                    if (_toggle)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        return 1;
+                    }
+                }
+                finally
+                {
+                    _toggle = !_toggle;
+                }
+            }
+        }
+    }
+}
---
+++ BitonicCountingNetwork/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BitonicCountingNetwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: implementation
+        }
+    }
+}
